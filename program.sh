0d26bb405e7dd6cc07c1dea5cd92ae83dc7fb130
physics <patomporn.pay@gmail.com>
2019-11-08 19:56:11 +0700

    backup

--- program.sh
+++ program.sh
@@ -4,7 +4,6 @@
 #SBATCH --partition long
 #SBATCH -o log_extractPhoton.out # Name of stdout output file (%j becomes %jobId)
 #SBATCH -N 1 # Total number of nodes requested
-#SBATCH -n 1 # Total number tasks per node
 #SBATCH -t 72:00:00 # Run time (hh:mm:ss) - 1.0 hours
 
 module load anaconda/2.7
