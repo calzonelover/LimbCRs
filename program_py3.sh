ff81edd7e69e55bb58148d20b62f71ebabc284d0
Patomp <patomporn.pay@gmail.com>
2019-11-21 16:08:07 +0700

    reextract photon to csv

--- program_py3.sh
+++ program_py3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #SBATCH --job-name=ExtractPhoton  ### Job Name
-#SBATCH --partition=long
+###SBATCH --partition=long
 #SBATCH --output=logExtractPhoton.out
-#SBATCH --time=70:00:00 ### WallTime (hh:mm:ss) format "hours:minutes:seconds"or "days-hours",
-#SBATCH --ntasks=92
+#SBATCH --time=24:00:00 ### WallTime (hh:mm:ss) format "hours:minutes:seconds"or "days-hours",
+#SBATCH --ntasks=148
 #SBATCH --account=jab ### Account used for job submission
 
 module load prun/1.3
--- unit_test/photon_converter/extract_pd_mpi.py
+++ unit_test/photon_converter/extract_pd_mpi.py
@@ -72,7 +72,7 @@ def main():
                 nadir_shift = zenith_to_nadir(zenith_shift)
                 if (
                     energy_gev >= settings.E_START_GEV and energy_gev <= settings.E_STOP_GEV
-                    and photon['THETA'] < settings.THETA_LAT_CUTOFF and photon['EVENT_CLASS'][-EVENT_CLASS_BITS['P8R2_SOURCE_V6']]
+                    and photon['THETA'] < settings.THETA_LAT_CUTOFF and photon['EVENT_CLASS'][-settings.EVENT_CLASS_BITS['P8R2_SOURCE_V6']]
                 ):
                     extracted_photons.append({
                         'time': photon['TIME'],
@@ -86,8 +86,8 @@ def main():
                         'altitude_km': sp['RAD_GEO']/1000.0,
                         'phi_earth': photon['EARTH_AZIMUTH_ANGLE'],
                         'rocking_angle': sp['ROCK_ANGLE'],
-                        'P8R2_SOURCE_V6': photon['EVENT_CLASS'][-EVENT_CLASS_BITS['P8R2_SOURCE_V6']],
-                        'P8R2_ULTRACLEANVETO_V6': photon['EVENT_CLASS'][-EVENT_CLASS_BITS['P8R2_ULTRACLEANVETO_V6']],
+                        'P8R2_SOURCE_V6': photon['EVENT_CLASS'][-settings.EVENT_CLASS_BITS['P8R2_SOURCE_V6']],
+                        'P8R2_ULTRACLEANVETO_V6': photon['EVENT_CLASS'][-settings.EVENT_CLASS_BITS['P8R2_ULTRACLEANVETO_V6']],
                     })
             df = pd.DataFrame(extracted_photons)
             df.to_csv(os.path.join(settings.PATH_EXTRACTED_DATA, 'photon', 'ft1_w%03d.csv'%week_i))
